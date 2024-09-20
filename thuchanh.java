9cffa55567e551d70ad4a406c87a15cf5815537f
Phamminhthuc <151825508+Phamminhthuc@users.noreply.github.com>
2024-09-20 14:35:44 +0700

    Add files via upload

---
+++ thuchanh.java
@@ -0,0 +1,46 @@
+public class thuchanh{
+    public static void main(String[] args) {
+       double height;
+       double weight;
+       double bmi;
+       
+       height=70;
+       weight=195;
+       bmi=weight/(height*height)*703;
+       System.out.println("gia tri BMI: ");
+       System.out.println(bmi);
+
+       double height2=70;
+       double weight2=195;
+       double bmi2=weight2/(height2*height2)*703;
+       System.out.println("gia tri BMI2:");
+       System.out.println(bmi2);
+       System.out.println();
+       
+       System.out.println("gia tri BMI2:" + bmi2);
+
+       double tinhtoan = (5*7.0/2-2.5)/5*2;
+       System.out.println("cau a: " + tinhtoan);
+       double tinhtoan1 = 41%7*3/5+5/2*2.5; 
+       System.out.println("cau b: " + tinhtoan1);
+       double tinhtoan2 = 8/5+13/2/3.0;
+       System.out.println("cau c: " + tinhtoan2);
+       double tinhtoan3 = (2.5+3.5)/2;
+       System.out.println("cau d: " + tinhtoan3);
+       double tinhtoan4 = 9/4*2.0-5/4;
+       System.out.println("cau e: " + tinhtoan4);
+       double tinhtoan5 = 9/2.0+7/3-3.0/2;
+       System.out.println("cau f: " + tinhtoan5);
+       double tinhtoan6 = 813%100/3+2.4;
+       System.out.println("cau g: " + tinhtoan6);
+       double tinhtoan7 = 27/2/2.0*(4.3+1.7)-8/3;
+       System.out.println("cau h: " + tinhtoan7);
+       double tinhtoan8 = 53/5/(0.6+1.4)/2+13/2;
+       System.out.println("cau i: " + tinhtoan8);
+       double tinhtoan9 = 2*3/4*2/4.0+4.5-1;
+       System.out.println("cau j: " + tinhtoan9);
+       double tinhtoan10 = 89%10/4*2.0/5+(1.5+1.0/2)*2;
+       System.out.println("cau k: " + tinhtoan10);
+     
+    }
+}
---
+++ thuchanh2.java
@@ -0,0 +1,12 @@
+public class thuchanh2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) 
+        {
+           //System.out.println(i+"Binh phuong la:" + (i*i));
+            
+           //System.out.println("+--------+");
+           //System.out.println("**********");
+        }
+    }
+    
+}
